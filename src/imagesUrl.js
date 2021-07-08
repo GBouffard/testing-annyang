2853b41fbe424cc92557ee3416a6cc11c5c24c92
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-07-08 15:20:11 +0200

    Add imagesUrl file

---
+++ src/imagesUrl.js
@@ -0,0 +1,9 @@
+const baseUrl = process.env.PUBLIC_URL;
+
+const imagesUrls = {
+  mickeyMouse: `${baseUrl}/images/mickeyMouse.jp`,
+  donaldDuck: `${baseUrl}/images/donaldDuck.jp`,
+  goofy: `${baseUrl}/images/goofy.jp`,
+};
+
+export { imagesUrls };
