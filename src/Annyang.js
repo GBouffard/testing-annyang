b598ae0dadfe99d0cc4c3896399f86de9fe5b86e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-07-08 11:25:59 +0200

    Configure Annyang to do what I want to do later

--- src/Annyang.js
+++ src/Annyang.js
@@ -1,56 +1,63 @@
-import annyang from 'annyang'
+import annyang from "annyang";
 
 class Annyang {
   // to check whether annyang is supported
   isSupported() {
-    return annyang !== null
+    return annyang !== null;
   }
 
   // to start voice recognition
   start() {
     if (annyang) {
-      annyang.start()
+      annyang.start();
     }
   }
 
   // to stop voice recognition
   abort() {
     if (annyang) {
-      annyang.abort()
+      annyang.abort();
     }
   }
 
   // to start voice recognition again from abort
   resume() {
     if (annyang) {
-      annyang.resume()
+      annyang.resume();
     }
   }
 
   // to predefine the command word(s) you want to say. Once annyang picks up those words, it will trigger the callback corresponding to each command
-  addCommands(reset, change, undo) {
+  addCommands(showMickeyMouse, showDonaldDuck, showGoofy) {
     if (annyang) {
       annyang.addCommands({
-        'reset': () => reset(),
-        'change': () => change(),
-        'undo': () => undo()
-      })
+        mickeyMouse: () => showMickeyMouse(),
+        donaldDuck: () => showDonaldDuck(),
+        goofy: () => showGoofy(),
+      });
     }
   }
 
   // where we get the voice engine status and voice input
   addCallback(engineCallback, resultCallback) {
     if (annyang) {
-      annyang.addCallback('start', event => engineCallback('on'))
-      annyang.addCallback('soundstart', event => engineCallback('listening'))
-      annyang.addCallback('end', event => engineCallback('off'))
-      annyang.addCallback('error', event => engineCallback(event.error))
-      annyang.addCallback('errorNetwork', event => engineCallback('network error'))
-      annyang.addCallback('errorPermissionBlocked', event => engineCallback('permission blocked'))
-      annyang.addCallback('errorPermissionDenied', event => engineCallback('permission denied'))
-      annyang.addCallback('result', event => resultCallback(event))
+      /* eslint-disable no-unused-vars */
+      annyang.addCallback("start", (event) => engineCallback("on"));
+      annyang.addCallback("soundstart", (event) => engineCallback("listening"));
+      annyang.addCallback("end", (event) => engineCallback("off"));
+      annyang.addCallback("error", (event) => engineCallback(event.error));
+      annyang.addCallback("errorNetwork", (event) =>
+        engineCallback("network error")
+      );
+      annyang.addCallback("errorPermissionBlocked", (event) =>
+        engineCallback("permission blocked")
+      );
+      annyang.addCallback("errorPermissionDenied", (event) =>
+        engineCallback("permission denied")
+      );
+      annyang.addCallback("result", (event) => resultCallback(event));
     }
   }
 }
 
-export default new Annyang()
+export default new Annyang();
--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
-import './App.css';
-import annyang from './Annyang'
+import React, { Component } from "react";
+import "./App.css";
+import annyang from "./Annyang";
 
-const App = () => {
-  return !annyang ? <div className="App">Speech Recognition is not supported</div> : <div className="App">Testing Annyang</div>
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      voiceStatus: "hello",
+    };
+  }
+
+  componentDidMount() {
+    // 1) bind with predefined words/commands on mount
+    annyang.addCommands(
+      this.showMickeyMouse,
+      this.showDonaldDuck,
+      this.showGoofy
+    );
+
+    // 2) add callbacks?
+    annyang.addCallback(this.engineCallback, this.resultCallback);
+
+    // 3) start Annyang
+    annyang.start();
+
+    this.setState({
+      voiceStatus: annyang.isSupported() ? "Supported" : "Unsupported",
+    });
+  }
+
+  // remove annyang on unmount
+  componentWillUnmount() {
+    annyang.abort();
+  }
+
+  // eslint-disable-next-line
+  engineCallback = (status) => {
+    // Set engine status
+  };
+
+  // eslint-disable-next-line
+  resultCallback = (voiceInput) => {
+    // Match voice input with player commands
+  };
+
+  showMickeyMouse = () => {
+    window.alert("this will show Mickey Mouse");
+  };
+
+  showDonaldDuck = () => {
+    window.alert("this will show Donald Duck");
+  };
+
+  showGoofy = () => {
+    window.alert("this will show Goofy");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {!annyang ? "Speech Recognition is not supported" : "Testing Annyang"}
+        <div>Annyang status: {this.state.voiceStatus.toUpperCase()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
